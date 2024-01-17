7d5dc62fc014a060b8f5f22f9f88e959872eb9bf
diskala <143798335+diskala@users.noreply.github.com>
2024-01-17 15:51:04 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { Navigate } from 'react-router-dom';
 import Components from './Components/Components';
-import Homes from './Pages/Homes/Homes';
+import Homes from './Pages/Homes/Home';
 import Competences from './Pages/Competences/Competences';
 import Portfolio from './Pages/Portfolio/Portfolio';
 import Contact from './Pages/Contact/Contact';
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
     < Components/>  
    <Routes>
+   <Route path="/" element={<Navigate to="/" />} />
     <Route path='/' element={<Homes/>}/>
     <Route path='/competences' element={<Competences/>}/>
     <Route path='/portfolio' element={<Portfolio/>}/>
