88cc2f138b27301d0ab2c42546f830e6e785e1a9
diskala <143798335+diskala@users.noreply.github.com>
2024-03-28 01:50:20 +0100

    correction composer.json

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App">
       
-      <BrowserRouter basename="/monportfolio">
+      <BrowserRouter basename="/portfolio">
       <Components />
       <Routes basename={process.env.PUBLIC_URL}>
         <Route path="/" element={<Homes />} />
